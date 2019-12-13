d50d40fead26cf03eba99732fdc0e8dcc22d61dd
Evan Greer <etgreer91@gmail.com>
2019-12-13 06:42:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,3 +57,15 @@ def reduce_to_total(source_array, starting_point = 0)
   
   total
 end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if !source_array[i]
+      return false
+    end
+    i += 1
+  end
+  
+  true
+end
