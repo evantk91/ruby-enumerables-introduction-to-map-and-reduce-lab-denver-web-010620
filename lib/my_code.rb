f927e3b9b0dbbd9f2f6c3db8cebead1e9282ab5a
Evan Greer <etgreer91@gmail.com>
2019-12-13 05:48:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,48 @@
 # My Code here....
+def map_to_negativize(array)
+  
+  i = 0
+  newArr = []
+  while i < array.length do
+    newArr << array[i] * -1
+    i += 1
+  end
+  
+  newArr
+end
+
+def map_to_no_change(array)
+  
+  i = 0
+  newArr = []
+  while i < array.length do
+    newArr << array[i]
+    i += 1
+  end
+  
+  newArr
+end
+
+def map_to_double(array)
+  
+  i = 0
+  newArr = []
+  while i < array.length do
+    newArr << array[i] * 2
+    i += 1
+  end
+  
+  newArr
+end
+
+def map_to_square(array)
+  
+  i = 0
+  newArr = []
+  while i < array.length do
+    newArr << array[i] * array[i]
+    i += 1
+  end
+  
+  newArr
+end
